fda2d8ffde4b0ed817211285a01bf7c3e19d7814
Fortzy05 <omorodion.fortune@gmail.com>
2024-08-20 17:32:26 +0100

    waiting list

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,200 @@
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
+import Footer from "./components/Footer";
+import Link from "next/link";
+import logo from "../assets/menu-nav-logo-1.png"
+export default function Page() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    phoneNumber: "",
+    countryCity: "",
+    email: "",
+    socialHandle: "",
+  });
 
-export default function Home() {
+  const [errors, setErrors] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const validate = () => {
+    const errors = {};
+    if (!formData.fullName) errors.fullName = "Full Name is required";
+    if (!formData.phoneNumber) errors.phoneNumber = "Phone Number is required";
+    if (!formData.countryCity) errors.countryCity = "Country/City is required";
+    if (!formData.email) errors.email = "Email is required";
+    if (!formData.socialHandle)
+      errors.socialHandle = "Social Handle is required";
+    return errors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+    setErrors(validationErrors);
+    if (Object.keys(validationErrors).length === 0) {
+      setIsSubmitted(true);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="w-screen min-h-screen">
+      <div className="bg-[#E3B522] gap-4 w-[100vw] h-[104px] items-center flex pl-[92px]">
+        <Image src={logo} height={80} width={80} alt="" />
+        <h1 className="uppercase font-display font-bold text-[25px] leading-[4rem] text-[#14BDE3]">
+          Kgf cause shop
+        </h1>
+      </div>
+
+      {isSubmitted ? (
+        <div className="px-24 py-8 my-8">
+          <div className="flex justify-center items-center">
+            <div className="w-[300px] h-[300px] bg-[#94D9B9]/15 rounded-full relative flex justify-center items-center ">
+              <div className="absolute w-[200px] h-[200px] bg-[#94D9B9]/45 rounded-full">
+                <div className="flex h-full mx-auto justify-center items-center ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="#00A859"
+                    className="size-[89.25px]"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h1 className="font-bold my-8 font-display text-[2rem] text-[#000000] text-center mt-4">
+            Congratulations!
+          </h1>
+          <h1 className="text-[20px] font-display text-[#6E6363] text-center mt-4">
+            You have been successfully added to the waiting list
+          </h1>
+          <Link className="h-full flex" href="/">
+            <button className="px-5 py-2.5 font-display bg-[#E3B522] rounded-[50px] text-[#f1f1f1f1] text-[20px] mt-11 mx-auto">
+              Go to the KGF Cause Shop
+            </button>
+          </Link>
         </div>
+      ) : (
+        <section className="px-24 pt-24 font-display">
+          <h1 className="my-4 font-display font-bold text-[4rem] leading-[4rem] text-[#000000]">
+            Welcome!
+          </h1>
+          <h4 className="my-8 font-display font-bold leading-10 text-[32px] text-[#000000]">
+            This website is under construction
+          </h4>
+          <p className="font-bold leading-[28px] text-[24px] text-[#6E6363]">
+            Join the waiting list for access to the carefully curated
+            ethically-sourced collection of African products
+          </p>
+          <div className=" my-24 flex justify-center">
+            <Image src="/cuate.svg" height={500} width={500} alt="" />
+          </div>
+          <div className="bg-[#F7F9F9] font-display flex items-center w-[940px] h-[112px] rounded-[10px] p-5">
+            <p className="text-[1rem] font-display leading-6 w-[900px] h-[72px] ">
+              By submitting this form, you consent to the collection and
+              processing of your personal data in accordance with the General
+              Data Protection Regulation (GDPR) for the purpose of the Research.
+              Your information and responses will be kept confidential and used
+              solely for the purpose of this research.
+            </p>
+          </div>
+
+          <div className="mt-11">
+            <form className="mb-[157px] font-display" onSubmit={handleSubmit}>
+              <div className="flex gap-5 mb-5 items-center">
+                <input
+                  className="rounded-[10px] border-[1px] border-[#6E6363] px-2.5 py-2.5 w-[380px] h-[34px]"
+                  type="text"
+                  name="fullName"
+                  placeholder="Full Name"
+                  value={formData.fullName}
+                  onChange={handleChange}
+                />
+                {errors.fullName && (
+                  <p className="text-red-500">{errors.fullName}</p>
+                )}
+                <input
+                  className="rounded-[10px] border-[1px] border-[#6E6363] px-2.5 py-2.5 w-[380px] h-[34px]"
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  value={formData.phoneNumber}
+                  onChange={handleChange}
+                />
+                {errors.phoneNumber && (
+                  <p className="text-red-500">{errors.phoneNumber}</p>
+                )}
+              </div>
+              <div className="flex gap-5 mb-5 items-center">
+                <input
+                  className="rounded-[10px] border-[1px] border-[#6E6363] px-2.5 py-2.5 w-[380px] h-[34px]"
+                  type="text"
+                  name="countryCity"
+                  placeholder="Country/City"
+                  value={formData.countryCity}
+                  onChange={handleChange}
+                />
+                {errors.countryCity && (
+                  <p className="text-red-500">{errors.countryCity}</p>
+                )}
+                <input
+                  className="rounded-[10px] border-[1px] border-[#6E6363] px-2.5 py-2.5 w-[380px] h-[34px]"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+                {errors.email && <p className="text-red-500">{errors.email}</p>}
+              </div>
+              <div className="mb-5 flex items-center">
+                <input
+                  className="rounded-[10px] border-[1px] border-[#6E6363] px-2.5 py-2.5 w-[788px] h-[34px]"
+                  type="url"
+                  name="socialHandle"
+                  placeholder="Twitter/Instagram/Facebook handle"
+                  value={formData.socialHandle}
+                  onChange={handleChange}
+                />
+                {errors.socialHandle && (
+                  <p className="text-red-500">{errors.socialHandle}</p>
+                )}
+              </div>
+              <button
+                className="px-5 py-2.5 bg-[#E3B522] text-[#ffffff] rounded-[50px]"
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </section>
+      )}
+
+      <div className="pb-2 relative mr-8 mb-8 ">
+        <h1 className="font-semibold text-[20px] pt-2 leading-7 w-[250px] right-11 absolute  text-[#6E6363]">
+          452 others just joined
+        </h1>
+        <div className="right-0 border-[#FFFFFF] rounded-full absolute w-[36px] h-[36px] border-[3px] bg-[#D9D9D9]"></div>
+        <div className="right-3 border-[#FFFFFF] rounded-full absolute w-[36px] h-[36px] border-[3px] bg-[#D9D9D9]"></div>
+        <div className="right-6 border-[#FFFFFF] rounded-full absolute w-[36px] h-[36px] border-[3px] bg-[#D9D9D9]"></div>
+        <div className="right-9 border-[#FFFFFF] rounded-full absolute w-[36px] h-[36px] border-[3px] bg-[#D9D9D9]"></div>
+        <div className="right-12 border-[#FFFFFF] rounded-full absolute w-[36px] h-[36px] border-[3px] bg-[#D9D9D9]"></div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="pt-11">
+        <Footer />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+       fontFamily: {
+        display: 'Calibri', // Adds a new `font-display` class
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
